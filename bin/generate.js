934912a7b2c3bc8bb52b1abc02a37c32fb434e05
Kaciras <Kaciras@protonmail.com>
2021-05-12 22:13:43 +0800

    refactor

--- bin/generate.js
+++ bin/generate.js
@@ -1,10 +1,27 @@
 import fs from "fs";
-import { ruleFile } from "../lib/source.js";
+import { gfwlist, ruleFile } from "../lib/source.js";
 import { ensureDirectory, projectFile } from "../lib/utils.js";
 import { buildPac } from "../lib/generator.js";
 
-ruleFile(projectFile("user-rules.txt")).then(buildPac).then(async result => {
-	const out = projectFile("dist/pac.js");
+const sources = [
+	gfwlist(),
+	ruleFile(projectFile("user-rules.txt")),
+];
+
+async function getOptions() {
+	const results = await Promise.all(sources);
+	const domains = results.flat();
+
+	return {
+		direct: "DIRECT",
+		rules: {
+			"SOCKS5 localhost:2080": domains,
+		},
+	};
+}
+
+getOptions().then(buildPac).then(async result => {
+	const out = projectFile("dist/proxy.pac");
 	await ensureDirectory(out);
 	fs.writeFileSync(out, result, { encoding: "UTF8" });
 });
