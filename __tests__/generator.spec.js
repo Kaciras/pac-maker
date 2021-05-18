2bbd7d94756bf4565993dae3ae426dd0169798b3
Kaciras <Kaciras@protonmail.com>
2021-05-18 16:16:00 +0800

    fix tests

--- __tests__/generator.spec.js
+++ __tests__/generator.spec.js
@@ -7,15 +7,21 @@ import { root } from "../lib/utils";
 jest.useFakeTimers("modern");
 jest.setSystemTime(new Date(2021, 5, 17, 0, 0, 0, 0));
 
-const fixtures = join(root, "__tests__/fixtures");
-const stubPac = fs.readFileSync(join(fixtures, "proxy.pac"), "utf8");
+const fixture = join(root, "__tests__/fixtures/proxy.pac");
+const stubPac = fs.readFileSync(fixture, "utf8");
 
 it("should load PAC script", () => {
 	const { direct, proxies, rules, FindProxyForURL } = loadPac(stubPac);
 
+	expect(proxies).toEqual([
+		"HTTP [::1]:2080",
+		"SOCKS5 localhost:1080",
+	]);
+	expect(rules).toEqual({
+		"foo.bar": 0,
+		"example.com": 1,
+	});
 	expect(direct).toBe("DIRECT");
-	expect(proxies).toEqual(["SOCKS5 localhost:1080"]);
-	expect(rules).toEqual({ "example.com": 0 });
 
 	expect(FindProxyForURL("", "")).toBe("DIRECT");
 	expect(FindProxyForURL("", "com")).toBe("DIRECT");
@@ -25,11 +31,8 @@ it("should load PAC script", () => {
 
 it("should build PAC script", async () => {
 	const code = await buildPac({
-		direct: "DIRECT",
-		domains: {
-			"HTTP [::1]:2080": ["foo.bar"],
-			"SOCKS5 localhost:1080": ["example.com"],
-		},
+		"HTTP [::1]:2080": ["foo.bar"],
+		"SOCKS5 localhost:1080": ["example.com"],
 	});
 	expect(code).toBe(stubPac);
 });
