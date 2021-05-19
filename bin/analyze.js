2530f075a444ab063d029672d6530aa6e128ab7f
Kaciras <Kaciras@protonmail.com>
2021-05-19 11:21:28 +0800

    fix table printing

--- bin/analyze.js
+++ bin/analyze.js
@@ -9,6 +9,7 @@ import { getAllBrowserHistories } from "../lib/history.js";
 process.chdir(root);
 
 const { path } = await getSettings();
+const file = "matches.json";
 
 const { FindProxyForURL } = loadPac(await fs.readFile(path, "utf8"));
 
@@ -16,9 +17,8 @@ const histories = await getAllBrowserHistories();
 const { rules, hostnameSet } = matchFindProxyFn(histories, FindProxyForURL);
 
 console.info(`Inspect ${histories.length} urls, ${hostnameSet.size} distinct hosts.`);
-const table = Object.fromEntries(Object.entries(rules).map(([k, v]) => [k, v.length]));
-console.table(table, ["matched domains"]);
+const table = Object.entries(rules).map(([k, v]) => ({ Proxy: k, "Matched Hosts": v.length }));
+console.table(table);
 
-const file = "matches.json";
 await fs.writeFile(file, JSON.stringify(rules, null, "\t"));
 console.info(`Rules saved to ${file}`);
