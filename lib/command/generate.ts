e6b72a348c6cdfe46d81164f50ec8803d20634c8
Kaciras <Kaciras@protonmail.com>
2022-03-01 19:00:06 +0800

    print changed hostname count

--- lib/command/generate.ts
+++ lib/command/generate.ts
@@ -1,11 +1,30 @@
-import { writeFile } from "fs/promises";
+import { readFileSync, writeFileSync } from "fs";
+import chalk from "chalk";
 import { PACMakerConfig } from "../config.js";
-import { buildPAC, HostnameListLoader } from "../generator.js";
+import { buildPAC, HostnameListLoader, HostRules } from "../generator.js";
 import { ensureDirectory } from "../utils.js";
+import { BuiltinPAC, loadPAC } from "../loader.js";
+
+const { redBright, greenBright } = chalk;
 
 interface CliOptions {
 	watch?: true;
 	config?: string;
+}
+
+function diff(file: string, newRules: HostRules) {
+	const { rules } = loadPAC<BuiltinPAC>(readFileSync(file, "utf8"));
+	const hosts = Object.values(newRules).flat();
+
+	let intersection = 0;
+	for (const host of hosts) {
+		if (host in rules) intersection += 1;
+	}
+
+	return {
+		added: hosts.length - intersection,
+		removed: Object.keys(rules).length - intersection,
+	};
 }
 
 export default async function (argv: CliOptions, config: PACMakerConfig) {
@@ -15,10 +34,21 @@ export default async function (argv: CliOptions, config: PACMakerConfig) {
 	await loader.refresh();
 
 	async function rebuildPACScript() {
-		const script = await buildPAC(loader.getRules(), direct);
+		const rules = loader.getRules();
+		const script = await buildPAC(rules, direct);
 		await ensureDirectory(path);
-		await writeFile(path, script, "utf8");
-		console.info("PAC script updated at " + new Date());
+
+		let detail = "";
+		try {
+			const { added, removed } = diff(path, rules);
+			detail += greenBright(` ${added}+`);
+			detail += redBright(`, ${removed}-.`);
+		} catch (e) {
+			// Old file is not exists or cannot parse
+		}
+
+		writeFileSync(path, script, "utf8");
+		console.log(`[${new Date()}] PAC updated.${detail}`);
 	}
 
 	await rebuildPACScript();
