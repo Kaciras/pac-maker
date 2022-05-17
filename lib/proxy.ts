28552e4156e3cbd642b99adf973b30a37c3d5577
Kaciras <Kaciras@protonmail.com>
2022-05-17 11:06:57 +0800

    correct cache proxy dispatcher

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -2,39 +2,19 @@ import tls from "tls";
 import { Agent, Dispatcher, ProxyAgent } from "undici";
 import { SocksClient } from "socks";
 import { DispatchHandlers, DispatchOptions } from "undici/types/dispatcher";
-import { FindProxy, loadPAC } from "./loader.js";
-import { SocksProxyType } from "socks/typings/common/constants";
-
-const re = /(\w+)(\s+([\w.]+)(?::(\d+))?)?;?/g;
-
-interface ParsedProxy {
-	hostname: string;
-	host: string;
-	port: number;
-	protocol: string;
-}
-
-function parseProxyURL(value: string) {
-	const result: ParsedProxy[] = [];
-	for (const match of value.matchAll(re)) {
-		const [, protocol, host2, hostname, sPort] = match;
-		const port = parseInt(sPort);
-		const host = host2.trimStart();
-		result.push({ protocol, host, hostname, port });
-	}
-	return result;
-}
+import { Callback, Options } from "undici/types/connector";
+import { FindProxy, loadPAC, ParsedProxy, parseProxies } from "./loader.js";
 
 function resolvePort(protocol: string, port: string) {
 	return port ? Number.parseInt(port) : protocol === "http:" ? 80 : 443;
 }
 
-export function socksConnector(
+function socksConnector(
 	socksHost: string,
 	socksPort: number,
-	version: SocksProxyType,
+	version: 4 | 5,
 ) {
-	return async (options: any, callback: any) => {
+	return async (options: Options, callback: Callback) => {
 		const { protocol, hostname, port } = options;
 		let { socket } = await SocksClient.createConnection({
 			proxy: {
@@ -45,7 +25,7 @@ export function socksConnector(
 			command: "connect",
 			destination: {
 				host: hostname,
-				port: resolvePort(protocol, port),
+				port: resolvePort(protocol, port as any),
 			},
 		});
 
@@ -64,6 +44,26 @@ export function socksConnector(
 	};
 }
 
+function createDispatcher(proxy: ParsedProxy): Dispatcher {
+	const { protocol, host, hostname, port } = proxy;
+	switch (protocol) {
+		case "DIRECT":
+			return new Agent();
+		case "SOCKS":
+		case "SOCKS5":
+			return new Agent({ connect: socksConnector(hostname, port, 5) });
+		case "SOCKS4":
+			return new Agent({ connect: socksConnector(hostname, port, 4) });
+		case "HTTP":
+		case "PROXY":
+			return new ProxyAgent(`http://${host}`);
+		case "HTTPS":
+			return new ProxyAgent(`https://${host}`);
+		default:
+			throw new Error(`Unknown proxy protocol: ${protocol}`);
+	}
+}
+
 export class PACDispatcher extends Dispatcher {
 
 	private readonly findProxy: FindProxy;
@@ -74,39 +74,31 @@ export class PACDispatcher extends Dispatcher {
 		this.findProxy = loadPAC(pac).FindProxyForURL;
 	}
 
-	dispatch(options: DispatchOptions, handler: DispatchHandlers) {
-		const key = options.origin!.toString();
+	async close() {
+		await Promise.all(this.mapAll(v => v.close()));
+	}
 
+	async destroy() {
+		await Promise.all(this.mapAll(v => v.destroy()));
+	}
+
+	private mapAll<T>(fn: (dispatcher: Dispatcher) => T) {
+		return Array.from(this.cache.values()).map(fn);
+	}
+
+	dispatch(options: DispatchOptions, handler: DispatchHandlers) {
+		const { path, origin } = options;
+
+		const p = this.findProxy(path, origin!.toString());
+		const [proxy] = parseProxies(p);
+
+		const key = `${proxy.protocol} ${proxy.host}`;
 		let dispatcher = this.cache.get(key);
 		if (!dispatcher) {
-			dispatcher = this.getDispatcher(options);
+			dispatcher = createDispatcher(proxy);
 			this.cache.set(key, dispatcher);
 		}
 
 		return dispatcher.dispatch(options, handler);
-	}
-
-	private getDispatcher(options: DispatchOptions): Dispatcher {
-		const { path, origin } = options;
-
-		const proxy = this.findProxy(path, origin!.toString());
-		const { protocol, host, hostname, port } = parseProxyURL(proxy)[0];
-
-		switch (protocol) {
-			case "DIRECT":
-				return new Agent();
-			case "SOCKS":
-			case "SOCKS5":
-				return new Agent({ connect: socksConnector(hostname, port, 5) });
-			case "SOCKS4":
-				return new Agent({ connect: socksConnector(hostname, port, 4) });
-			case "HTTP":
-			case "PROXY":
-				return new ProxyAgent(`http://${host}`);
-			case "HTTPS":
-				return new ProxyAgent(`https://${host}`);
-			default:
-				throw new Error(`Unknown proxy protocol: ${protocol}`);
-		}
 	}
 }
