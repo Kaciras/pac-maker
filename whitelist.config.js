1d0457c9cc00cd0d58098f6abaaf682d6bda7b5f
Kaciras <Kaciras@protonmail.com>
2022-07-16 15:22:12 +0800

    introduce dist/whitelist.pac

---
+++ whitelist.config.js
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+import { DnsmasqLists, PACDispatcher } from "./lib/index.js";
+
+const pac = readFileSync("dist/blacklist.pac", "utf8");
+const dispatcher = new PACDispatcher(pac);
+
+// The config used to generate dist/whitelist.pac
+export default {
+	path: "dist/whitelist.pac",
+	direct: "SOCKS5 localhost:2080",
+	sources: {
+		"DIRECT": [
+			new DnsmasqLists("accelerated-domains", { dispatcher }),
+		],
+	},
+};
