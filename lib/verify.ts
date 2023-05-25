a48e3f48cfa81fa014376819fd2fe96ffadc143a
Kaciras <Kaciras@protonmail.com>
2023-05-25 10:52:34 +0800

    refactor `verify` function to a class

--- lib/verify.ts
+++ lib/verify.ts
@@ -1,15 +1,12 @@
-import { BlockList, createConnection, IPVersion } from "net";
+import { BlockList, createConnection } from "net";
 import { resolve } from "dns/promises";
-import { fetch } from "undici";
+import { Dispatcher, fetch } from "undici";
 import { parseProxies } from "./loader.js";
 import { createAgent } from "./proxy.js";
 
-const blockedIPs = new BlockList();
-blockedIPs.addAddress("223.75.236.241"); // 反诈中心
-blockedIPs.addAddress("127.0.0.1");
-
-const TIMEOUT = 3_000;
-const CONCURRENCY = 10;
+const gfwIPs = new BlockList();
+gfwIPs.addAddress("223.75.236.241"); // 反诈中心
+gfwIPs.addAddress("127.0.0.1");
 
 export const blockType = Object.freeze({
 	dns: Symbol("DNS cache pollution"),
@@ -17,63 +14,73 @@ export const blockType = Object.freeze({
 	unavailable: Symbol("Can't access even with a proxy"),
 });
 
-async function connectTCP(host: string) {
+function connectTCP(host: string, timeout: number) {
 	const socket = createConnection({ host, port: 443 });
-	socket.setTimeout(TIMEOUT);
+	socket.setTimeout(timeout);
 
-	const connected = await new Promise(resolve => {
+	const task = new Promise(resolve => {
 		socket.on("connect", () => resolve(true));
 		socket.on("error", () => resolve(false));
 		socket.on("timeout", () => resolve(false));
 	});
 
-	const family = socket.remoteFamily as IPVersion;
-	const ip = socket.remoteFamily!;
-
-	socket.destroy();
-	return connected && !blockedIPs.check(ip, family);
+	return task.finally(() => socket.destroy());
 }
 
-const proxy = "SOCKS5 localhost:2080";
-const [p] = parseProxies(proxy);
-const dispatcher = createAgent(p);
+export class HostBlockVerifier {
 
-export function verify(hosts: string[]) {
-	const iterator = hosts[Symbol.iterator]();
-	const blocked: Record<string, symbol> = {};
+	private readonly dispatcher: Dispatcher;
+	private readonly timeout = 3000;
+	private readonly blockedIPs: BlockList;
 
-	async function doVerify(host: string) {
-		const [ip] = await resolve(host);
-		if (blockedIPs.check(ip)) {
-			return blocked[host] = blockType.dns;
-		}
+	constructor(proxy: string, blockedIPs = gfwIPs) {
+		this.blockedIPs = blockedIPs;
+		const [parsed] = parseProxies(proxy);
+		this.dispatcher = createAgent(parsed);
+	}
 
-		if (await connectTCP(host)) {
-			return;
+	async verify(host: string) {
+		const { dispatcher, blockedIPs, timeout } = this;
+
+		try {
+			const [ip] = await resolve(host);
+			if (blockedIPs.check(ip)) {
+				return blockType.dns;
+			}
+			if (await connectTCP(host, timeout)) {
+				return;
+			}
+		} catch {
+			return blockType.unavailable;
 		}
 
 		try {
-			await fetch(`"https://${host}`, { dispatcher });
-			blocked[host] = blockType.tcp;
+			await fetch(`https://${host}`, { dispatcher });
+			return blockType.tcp;
 		} catch {
-			blocked[host] = blockType.unavailable;
+			return blockType.unavailable;
 		}
 	}
 
-	async function run(): Promise<void> {
-		const { value, done } = iterator.next();
-		if (done) {
-			return;
-		}
-		if (value[0] === "*") {
-			return run();
-		}
-		return doVerify(value).then(run);
-	}
+	verifyAll(hosts: string[], concurrency = 10) {
+		const iterator = hosts[Symbol.iterator]();
+		const blocked: Record<string, symbol> = {};
 
-	const workers = [];
-	for (let i = 0; i < CONCURRENCY; i++) {
-		workers[i] = run();
+		const run = async (): Promise<void> => {
+			const { value, done } = iterator.next();
+			if (done) {
+				return;
+			}
+			if (value[0] === "*") {
+				return run();
+			}
+			return this.verify(value).then(run);
+		};
+
+		const workers = [];
+		for (let i = 0; i < concurrency; i++) {
+			workers[i] = run();
+		}
+		return Promise.all(workers).then(() => blocked);
 	}
-	return Promise.all(workers).then(() => blocked);
 }
