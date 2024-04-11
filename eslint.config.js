7932c1dfbd50374b03c771ed072f84769a9055a9
Kaciras <Kaciras@protonmail.com>
2024-04-11 09:15:33 +0800

    switch to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import core from "@kaciras/eslint-config-core";
+import typescript from "@kaciras/eslint-config-typescript";
+import jest from "@kaciras/eslint-config-jest";
+
+export default [
+	{ ignores: ["**/*.js", "!*.js", "!template/*.js"] },
+	...core,
+	...typescript,
+	{
+		rules: {
+			"kaciras/import-group-sort": "warn",
+		},
+	},
+	...jest.map(config => ({
+		...config,
+		files: ["test/**/*.ts"],
+	})),
+	{
+		files: ["template/*.js"],
+		rules: {
+			"@typescript-eslint/no-undef": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	},
+];
