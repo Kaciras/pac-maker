e25e2ec06da1c273ec8e3e42793c7a5d212098a8
Kaciras <contact@kaciras.com>
2025-06-03 15:13:18 +0800

    update deps

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,7 +1,6 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 export default {
 	packageManager: "pnpm",
-	plugins: ["@stryker-mutator/jest-runner"],
 	reporters: ["html", "progress"],
 	htmlReporter: {
 		fileName: "coverage/stryker.html",
@@ -9,7 +8,4 @@ export default {
 	disableTypeChecks: "**/*.ts",
 	ignoreStatic: true,
 	tempDirName: "stryker-tmp",
-	coverageAnalysis: "perTest",
-	testRunner: "jest",
-	testRunnerNodeArgs: ["--experimental-vm-modules"],
 };
